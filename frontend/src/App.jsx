5e62af06c8870dde8027f12884e9941a9885fe39
Ismael Ignacio Morinigo <thecoderbutcher@gmail.com>
2024-12-11 16:00:58 -0300

    feat; add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Doctors from "./pages/Doctors"
+import Login from "./pages/Login"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import MyProfile from "./pages/MyProfile"
+import MyAppoinments from "./pages/MyAppoinments"
+import Appoinment from "./pages/Appoinment"
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/doctors" element={<Doctors/>}/>
+        <Route path="/doctors/:speciality" element={<Doctors/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/my-profile" element={<MyProfile/>}/>
+        <Route path="/my-appoinment" element={<MyAppoinments/>}/>
+        <Route path="/appoinment/:docId" element={< Appoinment/>}/>
+      </Routes> 
+
     </div>
   )
 }
---
+++ frontend/src/pages/Appoinment.jsx
@@ -0,0 +1,8 @@
+
+const Appoinment = () => {
+    return (
+        <div>Appoinment</div>
+    )
+}
+
+export default Appoinment
